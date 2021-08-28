426ba7275e88b35327b80d8cb0f2d309c6ca922c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-08-28 19:09:00 +0000

    autogen: regenerate OpenAPI client for v1.10.6
    Version: v1.10.6

---
+++ ory_hydra_client/models/__init__.py
@@ -0,0 +1,53 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from ory_hydra_client.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from ory_hydra_client.model.accept_consent_request import AcceptConsentRequest
+from ory_hydra_client.model.accept_login_request import AcceptLoginRequest
+from ory_hydra_client.model.completed_request import CompletedRequest
+from ory_hydra_client.model.consent_request import ConsentRequest
+from ory_hydra_client.model.consent_request_session import ConsentRequestSession
+from ory_hydra_client.model.container_wait_ok_body_error import ContainerWaitOKBodyError
+from ory_hydra_client.model.flush_inactive_o_auth2_tokens_request import FlushInactiveOAuth2TokensRequest
+from ory_hydra_client.model.health_not_ready_status import HealthNotReadyStatus
+from ory_hydra_client.model.health_status import HealthStatus
+from ory_hydra_client.model.json_web_key import JSONWebKey
+from ory_hydra_client.model.json_web_key_set import JSONWebKeySet
+from ory_hydra_client.model.json_error import JsonError
+from ory_hydra_client.model.json_web_key_set_generator_request import JsonWebKeySetGeneratorRequest
+from ory_hydra_client.model.login_request import LoginRequest
+from ory_hydra_client.model.logout_request import LogoutRequest
+from ory_hydra_client.model.o_auth2_client import OAuth2Client
+from ory_hydra_client.model.o_auth2_token_introspection import OAuth2TokenIntrospection
+from ory_hydra_client.model.oauth2_token_response import Oauth2TokenResponse
+from ory_hydra_client.model.open_id_connect_context import OpenIDConnectContext
+from ory_hydra_client.model.patch_document import PatchDocument
+from ory_hydra_client.model.patch_request import PatchRequest
+from ory_hydra_client.model.plugin_config import PluginConfig
+from ory_hydra_client.model.plugin_config_args import PluginConfigArgs
+from ory_hydra_client.model.plugin_config_interface import PluginConfigInterface
+from ory_hydra_client.model.plugin_config_linux import PluginConfigLinux
+from ory_hydra_client.model.plugin_config_network import PluginConfigNetwork
+from ory_hydra_client.model.plugin_config_rootfs import PluginConfigRootfs
+from ory_hydra_client.model.plugin_config_user import PluginConfigUser
+from ory_hydra_client.model.plugin_device import PluginDevice
+from ory_hydra_client.model.plugin_env import PluginEnv
+from ory_hydra_client.model.plugin_interface_type import PluginInterfaceType
+from ory_hydra_client.model.plugin_mount import PluginMount
+from ory_hydra_client.model.plugin_settings import PluginSettings
+from ory_hydra_client.model.previous_consent_session import PreviousConsentSession
+from ory_hydra_client.model.reject_request import RejectRequest
+from ory_hydra_client.model.request_was_handled_response import RequestWasHandledResponse
+from ory_hydra_client.model.string_slice_pipe_delimiter import StringSlicePipeDelimiter
+from ory_hydra_client.model.userinfo_response import UserinfoResponse
+from ory_hydra_client.model.version import Version
+from ory_hydra_client.model.volume import Volume
+from ory_hydra_client.model.volume_usage_data import VolumeUsageData
+from ory_hydra_client.model.well_known import WellKnown
